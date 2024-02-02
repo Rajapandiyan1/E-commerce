a13c00eafcdc68dcd435ccfe8a4525373a5d96b1
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-02 21:40:44 +0530

    deploy add and root page add in E-commerce/

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
 <BrowserRouter>
 <Routes>
-  <Route path='/' element={<Home/>}/>
+  <Route path='E-commerce/' element={<Home/>}/>
 </Routes>
 </BrowserRouter>
     </div>
