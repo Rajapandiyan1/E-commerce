1c8e4f4d85e758fe64563c7d80650d9cbc4e49f0
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-03 21:50:38 +0530

    product navbar add in canvas in right in home

--- src/components/NavProduct.jsx
+++ src/components/NavProduct.jsx
@@ -21,8 +21,18 @@ function NavProduct() {
     </div>
         </div>
         <div className="col-2 ms-auto me-2 d-flex justify-content-center align-items-center" >
-            <i className="fa fa-bars" style={{fontSize:'25px'}}></i>
+            <i className="fa fa-bars" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" style={{fontSize:'25px'}}></i>
         </div>
+         
+<div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasBottomLabel1">Profile</h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body small">
+    
+  </div>
+</div>
     </div>
   )
 }
