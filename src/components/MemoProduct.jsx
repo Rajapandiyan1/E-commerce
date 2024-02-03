a40a72f76b65e179ae6f9ee7b6fa43bfebd2370e
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-03 15:40:53 +0530

    card width ajustment

--- src/components/MemoProduct.jsx
+++ src/components/MemoProduct.jsx
@@ -9,40 +9,40 @@ import camera from './images/camera.webp'
 export default function MemoProduct() {
   return (
     <div className='row me-0 ms-0 mt-3'>
-      <div className="col-sm-2 d-flex justify-content-center col-4 border">
-        <div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4">
+        <div className="card mt-1 mb-1 border w-100" style={{ height: '100px' }}>
           <div className="card-img-top">
             <img className='w-100' height='70px' src={mobile} alt="" />
           </div>
           <label htmlFor="" className='text-center memo-text form-check-label'>Mobiles</label>
         </div>
       </div>
-      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4"><div className="card mt-1 mb-1 w-100" style={{ height: '100px' }}>
         <div className="card-img-top">
           <img className='w-100' height='70px' src={sport} alt="" />
         </div>
         <label htmlFor="" className='text-center memo-text'>Sports</label>
       </div></div>
 
-      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4"><div className="card mt-1 mb-1 w-100" style={{ height: '100px' }}>
         <div className="card-img-top">
           <img className='w-100' height='70px' src={tv} alt="" />
         </div>
         <label htmlFor="" className='memo-text text-center'>Sports</label>
       </div></div>
-      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4"><div className="card mt-1 mb-1 w-100" style={{ height: '100px' }}>
         <div className="card-img-top">
           <img className='w-100' height='70px' src={camera} alt="" />
         </div>
         <label htmlFor="" className='memo-text text-center'>Camera</label>
       </div></div>
-      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1 w-100" style={{ height: '100px' }}>
         <div className="card-img-top">
           <img className='w-100' height='70px' src={headset} alt="" />
         </div>
         <label htmlFor="" className='text-center memo-text'>Headset</label>
       </div></div>
-      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1" style={{ height: '100px' }}>
+      <div className="col-sm-2 d-flex justify-content-center col-4 border"><div className="card mt-1 mb-1 w-100" style={{ height: '100px' }}>
         <div className="card-img-top">
           <img className='w-100' height='70px' src={laptop} alt="" />
         </div>
