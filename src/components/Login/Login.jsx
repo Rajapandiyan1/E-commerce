d96a2ae0c3f30ecac3b94bbf403cf00f5fd81ba5
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-04 15:57:34 +0530

    login page center alignment work

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -57,32 +57,33 @@ function Login() {
   return (
     <>
     <Navbar login={'Register'}/>
-    <div className='row justify-content-center mt-3 me-0 ms-0 ' style={{minHeight:'50vh'}}>
-<div className="col-12  d-flex align-items-center">
-<div className="row justify-content-center">
-<div className="col-12 col-md-7 text-center">
+<div className='row w-100 mt-3 me-0 ms-0 ' style={{minHeight:'50vh'}}>
+  <div className="col-12 d-flex justify-content-center">
+      <div className="row justify-content-center">
+      <div className="col-12 col-md-9 text-center">
         <h1>Login Page</h1>
       </div>
-      <div className="col-12 col-md-7 mt-4">
+      <div className="col-12 col-md-9 mt-4">
         <label htmlFor="" className='form-label'>Email</label>
       </div>
-      <div className="col-12 col-md-7 mt-3">
+      <div className="col-12 col-md-9 mt-3">
         <input type="text" placeholder='Enter your email' className='form-control' onChange={(e) => { getDatas(e) }} name="email" id="" />
       </div>
-      <div className="col-12 col-md-7 mt-2">
-        {err.email && <div className='col-12 col-md-7  mt-1 text-danger'>Invalid Email</div>}
+      {err.email && <div className='col-12 col-md-9  mt-1 text-danger'>Invalid Email</div>}
+
+      <div className="col-12 col-md-9 mt-2">
 
         <label htmlFor="" className='form-label'>Password</label>
       </div>
-      <div className="col-12 col-md-7 mt-3">
+      <div className="col-12 col-md-9 mt-3">
         <input type="password" placeholder='Enter your password' className='form-control' onChange={(e) => { getDatas(e) }} name="password" id="" />
       </div>
-      {err.password && <div className='col-12 col-md-7 mt-1 text-danger'>Invalid Password</div>}
-      {cusErr && <div className='col-12 col-md-7 mt-2'>{mess}</div>}
-      {loading && <div className="col-12 col-md-7 mt-5 d-flex justify-content-center">
+      {err.password && <div className='col-12 col-md-9 mt-1 text-danger'>Invalid Password</div>}
+      {cusErr && <div className='col-12 col-md-9 mt-2'>{mess}</div>}
+      {loading && <div className="col-12 col-md-9 mt-5 d-flex justify-content-center">
         <span className='spinner-border'></span>
       </div>}
-      <div className="col-12 col-md-7 mt-4">
+      <div className="col-12 col-md-9 mt-4">
         <div className="row">
         <div className="col-9 col-md-6 d-flex align-items-center">
         <span>create <Link style={{ textDecoration: 'none' }} to='/Register'>new account</Link></span>
@@ -93,9 +94,9 @@ function Login() {
         </div>
       </div>
 
+      </div>
+  </div>
 </div>
-</div>
-    </div>
     </>
   )
 }
