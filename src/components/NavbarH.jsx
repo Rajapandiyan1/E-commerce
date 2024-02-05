191084324317b9b30db417f81cb06064b3e6aa29
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-05 11:26:32 +0530

    home navbar work as admin page move

--- src/components/NavbarH.jsx
+++ src/components/NavbarH.jsx
@@ -13,11 +13,10 @@ export default function NavbarH() {
         fildatas(data)
     }
     useEffect(()=>{
-        alert('col-8')
     let token= localStorage.getItem('Dashboard');
     if(token != null){
         setadmin(true)
-        setinres('col-8')
+        setinres('col-8 col-sm-9')
     }
     else{
         setinres(false)
@@ -32,25 +31,25 @@ senddata(setData(fil))
     }
   return (
     <div className="row me-0 ms-0 border">
-        <div className="col-3 col-lg-2 order-1 ms-3 order-lg-1 ms-sm-1 mt-lg-0 justify-content-center align-items-center d-flex  navbar-brand text-warning mt-2">
+        <div className="memo-text-m ps-1 ps-sm-3 col-5 col-lg-2 order-1 order-lg-1  mt-lg-0 justify-content-start align-items-center d-flex   text-warning mt-2">
             E-commerce
         </div>
-        <div className="col-1 order-2 ms-auto col-lg-1 order-lg-3 order-lg-2 o mt-lg-0 justify-content-center align-items-center d-flex  me-5 mt-2">
+        <div className="col-2 ms-lg-auto ms-md-auto order-2 me-3 me-sm-0 col-lg-1 justify-content-md-end order-lg-3 order-lg-2 o mt-lg-0 justify-content-center align-items-center d-flex  mt-2">
             <button className="btn-success btn btn-sm" onClick={()=> {moveProduct('/Register')}}>
             Register</button>
             </div>
-        <div className="col-1 col-lg-1 order-3 order-lg-4 mt-lg-0 justify-content-center align-items-center  d-flex me-4 mt-2">
+        <div className="col-2 col-lg-1 ms-lg-auto order-3 me-lg-4 order-lg-4 mt-lg-0 justify-content-md-end justify-content-center align-items-center  d-flex mt-2">
             <button className="btn btn-primary btn-sm" onClick={()=> {moveProduct('/login')}}>
             Login</button></div>
-            {admin && <div className="col-4 d-none d-lg-flex col-sm-2 col-lg-2 mt-lg-1 mb-lg-1 order-lg-5 ps-0 justify-content-center order-6 mt-1  d-flex align-items-center">
+            {admin && <div className="col-4 ms-auto d-none d-lg-flex col-sm-2 col-lg-1 mt-lg-1 mb-lg-1 order-lg-5 ps-0 justify-content-sm-end justify-content-center order-6 mt-1  d-flex align-items-center">
             <button onClick={()=>{moveProduct('/admin') }} className="btn btn-primary btn-sm memo-text">
                 Dashboard
             </button>
         </div>}
-            <div className="col-1 col-lg-1 order-lg-5 mt-lg-1 mb-lg-1 order-4 mt-lg-0 me-2 mt-2 d-flex justify-content-center align-items-center">
+            <div className="col-2 col-lg-1 ms-lg-auto pe-md-5 order-lg-5 mt-lg-1 mb-lg-1 order-4 mt-lg-0  mt-2 d-flex justify-content-center justify-content-sm-end align-items-center">
             <i className="fa fa-bars" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" style={{fontSize:'24px'}}></i>
             </div>
-        <div className={'col-lg-10 col-lg-4  order-lg-2 order-5 mb-lg-1 mt-lg-1 justify-content-center align-items-center d-flex  mt-3 mb-2 '+inres}>
+        <div className={' col-lg-4    order-lg-2 order-5 mb-lg-1 mt-lg-1 justify-content-center align-items-center d-flex  mt-3 mb-2 '+inres}>
             <div className="input-group">
                 <input type="search" placeholder='search products' onChange={(e)=>{fildata(e.target.value)}} className='form-control' name="" id="" />
                 <div className="input-group-text">
@@ -59,8 +58,8 @@ senddata(setData(fil))
             </div>
 
         </div>
-        {admin && <div className="col-4 d-lg-none col-sm-2 col-lg-2 mt-lg-1 mb-lg-1 order-lg-5 ps-0 justify-content-center order-5 mt-1  d-flex align-items-center">
-            <button onClick={()=>{moveProduct('/admin') }} className="btn btn-primary ms-auto btn-sm memo-text">
+        {admin && <div className="col-4 ms-sm-auto d-lg-none col-sm-3 ms-lg-auto col-lg-1 mt-lg-1 mb-lg-1 order-lg-5 ps-0 justify-content-end pe-sm-5 order-5 mt-1  d-flex align-items-center">
+            <button onClick={()=>{moveProduct('/admin') }} className="btn btn-primary btn-sm memo-text">
                 Dashboard
             </button>
         </div>}
