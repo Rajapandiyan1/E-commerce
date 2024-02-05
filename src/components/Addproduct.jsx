ef7a46083a9b56b316d71417cde9f79dbe8654c3
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-05 21:10:25 +0530

    product add in webpage componet added

--- src/components/Addproduct.jsx
+++ src/components/Addproduct.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
 
 export default  function Addproduct() {
-  let [data,setdata]=useState({product:'',url:'',rate:'',offer:'',discountRate:''});
-   let [output,setoutput]=useState(false)
+  let [data,setdata]=useState({product:'',url:'',rate:'',offer:'',discountRate:0});
+   let [output,setoutput]=useState(false);
+   let [clone,setclone]=useState({product:'',url:'',rate:'',offer:'',discountRate:0})
   function calculate(e) {
     setdata((prev)=>{ return {...prev,offer:e.target.value}});
     setdata((prev)=>{return {...prev,discountRate:prev.rate*prev.offer/100}})
   }
   function Submits(params) {
-     setoutput(true)
+    setclone(data)
+    setdata({product:'',url:'',rate:'',offer:'',discountRate:0})
+    if(data.offer != '' && data.product != '' &&data.rate != '' &&data.url != ''){
+      setoutput(true)
+    }
+  }
+  function uploadProduct(params) {
+    alert('Upload success')
   }
   return (
     <div className="row justify-content-center">
@@ -19,20 +27,20 @@ export default  function Addproduct() {
               <label htmlFor="" className='form-label'>Product Name</label>
             </div>
             <div className="col-12 mt-3">
-              <input type="text" onChange={(e)=>{setdata((prev)=>{ return {...prev,product:e.target.value}})}} placeholder='Enter Product Name' className='form-control' name="" id="" />
+              <input type="text" value={data.product}  onInput={(e)=>{setdata((prev)=>{ return {...prev,product:e.target.value}})}} placeholder='Enter Product Name' className='form-control' name="" id="" />
             </div>
             <div className="col-12 mt-3">
               <label htmlFor="" className='form-label'>Product Img Url</label>
             </div>
             <div className="col-12 mt-3">
-              <input type="text" onChange={(e)=>{setdata((prev)=>{ return {...prev,url:e.target.value}})}} placeholder='Enter Image url' name="" id="" className="form-control" />
+              <input type="text" value={data.url} onInput={(e)=>{setdata((prev)=>{ return {...prev,url:e.target.value}})}} placeholder='Enter Image url' name="" id="" className="form-control" />
             </div>
             <div className="col-4 mt-3"><label htmlFor="" className='form-label'>Rate </label></div>
             <div className="col-4 mt-3"><label htmlFor="" className='form-label'>Offer Percentage </label></div>
             <div className="col-4 mt-3"><label htmlFor="" className='form-label'>Offer Rate </label></div>
-            <div className="col-4 mt-3"><input onChange={(e)=>{setdata((prev)=>{ return {...prev,rate:e.target.value}})}} type="number" className='form-control' placeholder='Enter Product Rate' name="" id="" /></div>
-            <div className="col-4 mt-3"><input onInput={(e)=>{ calculate(e)}} type="number" minLength={0} maxLength={100} className='form-control' placeholder='Enter Product Rate' name="" id="" /></div>
-            <div className="col-4 mt-3"><input type="text"value={data.discountRate} disabled={true} className='form-control' placeholder='0' name="" id="" /></div>
+            <div className="col-4 mt-3"><input onInput={(e)=>{setdata((prev)=>{ return {...prev,rate:e.target.value}})}} type="number" value={data.rate} className='form-control' placeholder='Enter Product Rate' name="" id="" /></div>
+            <div className="col-4 mt-3"><input onInput={(e)=>{ calculate(e)}} type="number" minLength={0} maxLength={100} value={data.offer} className='form-control' placeholder='Enter Product Rate' name="" id="" /></div>
+            <div className="col-4 mt-3"><input  type="text"value={data.discountRate} disabled={true} className='form-control' placeholder='0' name="" id="" /></div>
           </div>
           <div className="col-12 mt-3 d-flex justify-content-end ">
             <button className="btn btn-primary me-5" onClick={()=>{Submits()}}>
@@ -42,20 +50,24 @@ export default  function Addproduct() {
        {output && <div className="col-12 col-md-6">
           <div className="row">
             <div className="col-4">
-<img src={data.url} alt="" height='100px'/>
+<img src={clone.url} alt="" height='100px'/>
             </div>
             <div className="col-8">
               <div className="row">
                 <div className="col-12">
-                  {data.product}
+                 <span>Product name : </span> {clone.product}
                 </div>
                 <div className="col-12">
-                  <div>{data.rate}</div><div>{data.offer}</div><div>{data.discountRate}</div>
+                  <div><span>Product Rate : </span>{clone.rate}</div><div><span>Offer percentage : </span>{clone.offer} % </div><div><span>Discount Rate : </span>{clone.discountRate}</div>
                 </div>
               </div>
             </div>
+            <div className="col-12 d-flex mb-5  justify-content-end">
+              <button className="btn btn-success me-4 btn-sm" onClick={()=>{uploadProduct()}}>Upload Product</button>
+            </div>
           </div>
         </div>}
+        
         </div>
     </div>
 
