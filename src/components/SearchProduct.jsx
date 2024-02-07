5d16b7521948d5a092415d8017cea2a9e8bd66c0
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-07 20:00:28 +0530

    product resposive img set

--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -7,19 +7,19 @@ export default function SearchProduct({prud,load}) {
         <span className="spinner-border text-success"></span>
         </div>}
       { !load &&
-      prud.map((data,id)=>{return <div className="row border" key={id} style={{height:'250px'}}>
-      <div className="col-4">
-        <img src={data.url} className='w-100' height={'140px'} alt="" />
+      prud.map((data,id)=>{return <div className="row border" key={id} style={{minHeight:'80px'}}>
+      <div className="col-5 d-flex align-items-center col-md-3 col-sm-4 col-lg-3">
+        <img src={data.url} className='w-100 ratio ratio-4x2'  alt="" />
       </div>
       
-      <div className="col-8">
-      <div className='mt-1'><span>Product  : </span>{data.product}</div>
+      <div className="col-7 col-md-9 col-sm-8 col-lg-9">
+      <div className='memo-text-1'><span className='memo-text-1'>Product  : </span>{data.product}</div>
 
-                  <div className='mt-1'><span>Product Rate : </span>{data.rate}</div>
+                  <div className='mt-1 memo-text-1'><span className='memo-text-1'>Product Rate : </span>{data.rate}</div>
                   <div 
-                  className='mt-1'><span>Offer percentage : </span>{data.offer} % </div><div className='mt-1'><span>Discount Rate : </span>{data.discountRate}</div>
+                  className='mt-1 memo-text-1'><span className='memo-text-1'>Offer percentage : </span>{data.offer} % </div><div className='mt-1 memo-text-1'><span className='memo-text-1'>Discount Rate : </span>{data.discountRate}</div>
                 </div>
-                <div className="row mb-3 justify-content-end">
+                <div className="row mb-2 mt-2 justify-content-end">
                   <div className="col-4">
                 <button className="btn btn-primary  btn-sm w-100">Buy</button>
                   </div>
